73f42ee3ebaf381cba84db8aed72a902d04a837f
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-01-12 10:48:42 -0300

    faz as mudanças na classe principal pra carregar com os novos pacotes

--- src/main/java/br/com/ferdbgg/App.java
+++ src/main/java/br/com/ferdbgg/App.java
@@ -17,17 +17,18 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Parent parent = loadFXML("primary");
+        scene = new Scene(parent, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("fxmls/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
