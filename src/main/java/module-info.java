c4d4749238559e7420858590def77deb0cad061c
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-01-12 10:48:04 -0300

    muda configuração para encontrar os controllers no novo pacote

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module br.com.ferdbgg {
+    
     requires javafx.controls;
     requires javafx.fxml;
 
     opens br.com.ferdbgg to javafx.fxml;
     exports br.com.ferdbgg;
+
+    opens br.com.ferdbgg.controllers to javafx.fxml;
+    exports br.com.ferdbgg.controllers;
+
 }
